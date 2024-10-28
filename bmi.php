644ed6a0e3f0605a27eb8442211c547759dd5a38
mackliu <macklun@gmail.com>
2024-10-28 14:07:33 +0800

    建置首頁引導連結

--- bmi.php
+++ bmi.php
@@ -6,6 +6,9 @@
     <title>計算BMI</title>
 </head>
 <body>
+    <div>
+        <a href="index.html">回首頁</a>
+    </div>
     <?php 
     if(isset($_GET['bmi'])){
         echo "你上一次量測的BMI為{$_GET['bmi']}";
